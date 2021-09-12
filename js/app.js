0cbe73f01e5971dabff3b8c2fb9b64e10282f75e
imprantu <rakinhasanbabu4@gmail.com>
2021-09-13 01:45:56 +0600

    card position in row start fixed and images not shown fixed

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
